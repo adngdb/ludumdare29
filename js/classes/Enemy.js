243cf979404ce8f5f0859d6cf9b97ec462b9fb33
Adrian Gaudebert <adrian@gaudebert.fr>
2014-04-26 18:09:27 +0200

    Enemies can attack a player and can die.

--- js/classes/Enemy.js
+++ js/classes/Enemy.js
@@ -4,10 +4,16 @@ App.Enemy = function(game, x, y, target) {
     this.game = game;
     this.target = target;
 
-    this.anchor.setTo(0.5, 0.5);
-    this.SPEED = 60;
+    this.SPEED = 60; // in pixels per second
+    this.REACH_DISTANCE = 40; // in pixels
+    this.DAMAGES_TO_PLAYER = 10; // in health points
+    this.DAMAGES_TO_TOWER = 5; // in health points
+    this.ATTACK_COOLDOWN = 2; // in seconds
 
     this.game.physics.enable(this, Phaser.Physics.ARCADE);
+    this.anchor.setTo(0.5, 0.5);
+
+    this.lastAttack = null;
 };
 
 // Enemies are a type of Phaser.Sprite
@@ -18,5 +24,28 @@ App.Enemy.prototype.update = function() {
     // This enemy moves towards the player constantly. It won't try to
     // attack towers.
 
-    this.game.physics.arcade.moveToObject(this, this.target, this.SPEED);
+    // If the enemy is close enough to its target.
+    if (this.game.physics.arcade.distanceBetween(this, this.target) <= this.REACH_DISTANCE) {
+        // Stop movement.
+        this.body.velocity.x = 0;
+        this.body.velocity.y = 0;
+
+        // Attack the target if the cooldown time has passed.
+        if (!this.lastAttack || this.game.time.elapsedSecondsSince(this.lastAttack) > this.ATTACK_COOLDOWN) {
+            this.target.hurt(this.DAMAGES_TO_PLAYER);
+            this.lastAttack = this.game.time.now;
+        }
+    }
+    else {
+        // Move towards the target.
+        this.game.physics.arcade.moveToObject(this, this.target, this.SPEED);
+    }
+};
+
+App.Enemy.prototype.hurt = function (damages) {
+    this.health -= damages;
+
+    if (this.health <= 0) {
+        this.kill();
+    }
 };
--- js/classes/Player.js
+++ js/classes/Player.js
@@ -1,8 +1,7 @@
-App.Player = function(game, x, y) {
+App.Player = function (game, x, y) {
     Phaser.Sprite.call(this, game, x, y, 'player');
 
     this.game = game;
-    this.isInConstructMode;
 
     this.anchor.setTo(0.5, 0.5);
 
@@ -11,17 +10,21 @@ App.Player = function(game, x, y) {
     // Enable physics on the player
     this.game.physics.enable(this, Phaser.Physics.ARCADE);
     this.body.immovable = true;
+
+    // Health of the player.
+    // The game is lost is that number goes under zero.
+    this.health = 100;
 };
 
 // Player is a type of Phaser.Sprite
 App.Player.prototype = Object.create(Phaser.Sprite.prototype);
 App.Player.prototype.constructor = App.Player;
 
-App.Player.prototype.activateConstructMode = function() {
+App.Player.prototype.activateConstructMode = function () {
     this.isInConstructMode = true;
 };
 
-App.Player.prototype.deactivateConstructMode = function() {
+App.Player.prototype.deactivateConstructMode = function () {
     this.isInConstructMode = false;
 };
 
@@ -31,4 +34,12 @@ App.Player.prototype.collisionWithEnemy = function() {
 
 App.Player.prototype.collisionWithTower = function() {
     console.log('collisionWithTower');
+};
+
+App.Player.prototype.hurt = function (damages) {
+    this.health -= damages;
+
+    if (this.health <= 0) {
+        console.log('noooooooooooooo!');
+    }
 };
