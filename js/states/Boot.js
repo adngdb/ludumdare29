4b53b227fcb5d8a14227a489c11c1342fc8de3e8
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-04-26 16:17:07 +0200

    Allow visibility change to toggle pause in game

--- js/states/Boot.js
+++ js/states/Boot.js
@@ -20,7 +20,7 @@ App.Boot.prototype = {
         this.input.maxPointers = 1;
 
         //  Phaser will automatically pause if the browser tab the game is in loses focus. You can disable that here:
-        this.stage.disableVisibilityChange = true;
+        this.stage.disableVisibilityChange = false;
 
         if (this.game.device.desktop)
         {
