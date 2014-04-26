6dffb8b5e38a7b4d491b7cc2d8ec1f9c054e42d8
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-04-26 12:43:31 +0200

    Resize images to 1000*800

--- js/states/Preloader.js
+++ js/states/Preloader.js
@@ -7,7 +7,7 @@ App.Preloader.prototype = {
 
     preload: function() {
         this.background = this.add.sprite(0, 0, 'preloaderBackground');
-        this.preloadBar = this.add.sprite(this.world.width - (this.world.width / 2) - 150, 50, 'preloaderBar');
+        this.preloadBar = this.add.sprite(this.world.centerX - 150, this.world.centerY - 60, 'preloaderBar');
 
         this.load.setPreloadSprite(this.preloadBar);
 
